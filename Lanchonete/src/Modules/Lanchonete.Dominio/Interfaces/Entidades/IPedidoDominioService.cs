30e381c522646acab28821310a43550319e8a01e
Fernando Mendes <fmendes@mmi.com.br>
2019-03-27 16:59:09 -0300

    Criando serviços

---
+++ Lanchonete/src/Modules/Lanchonete.Dominio/Interfaces/Entidades/IPedidoDominioService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Lanchonete.Dominio.Modelos;
+
+namespace Lanchonete.Dominio.Interfaces.Entidades
+{
+    public interface IPedidoDominioService
+    {
+        Task InserirAsync(Pedido pedido);
+    }
+}
